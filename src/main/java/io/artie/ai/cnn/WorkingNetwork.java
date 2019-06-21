3ce56f5bb28f4dbba8e8963e6d5d50cd15e483a3
David (Xiaoyu) Wang <XWang@Scholastic.com>
2019-06-20 20:22:31 -0400

    enhanced version

--- src/main/java/io/artie/ai/cnn/WorkingNetwork.java
+++ src/main/java/io/artie/ai/cnn/WorkingNetwork.java
@@ -1,12 +1,11 @@
 package io.artie.ai.cnn;
 
-import java.io.BufferedInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -15,44 +14,24 @@ import org.supercsv.prefs.CsvPreference;
 
 public class WorkingNetwork {
 
-	private ArrayList<ArrayList<Double>> weights = new ArrayList<ArrayList<Double>>();
-	private ArrayList<ArrayList<Double>> vals = new ArrayList<ArrayList<Double>>();
-	private ArrayList<ArrayList<Double>> deltas = new ArrayList<ArrayList<Double>>();
+	private static final int TRAINING_ROUNDS = 1000;
+	private List<List<Double>> weights = new ArrayList<List<Double>>();
+	private List<List<Double>> vals = new ArrayList<List<Double>>();
+	private List<List<Double>> deltas = new ArrayList<List<Double>>();
 
-	private ArrayList<Double> inputs = new ArrayList<>();
-	private ArrayList<Double> targets = new ArrayList<>();
-	private ArrayList<Double> outputs = new ArrayList<>();
-
-	private ArrayList<ArrayList<Double>> trainingData = new ArrayList<ArrayList<Double>>();
-	private ArrayList<Double> pairedTargets = new ArrayList<>();
+	private List<Double> targets = new ArrayList<>();
+	private List<List<Double>> trainingData = new ArrayList<List<Double>>();
+	private List<Double> pairedTargets = new ArrayList<>();
 
 	private int numLayers;
-	Scanner scnr = new Scanner(System.in);
+	private Scanner scnr = new Scanner(System.in);
+	private int[] neuronsInHiddenLayers = new int[] { 20, 15, 10 };
 
 	public static void main(String[] args) throws IOException {
+		int numberOfLayers = 3;
+		WorkingNetwork nn = new WorkingNetwork(numberOfLayers);
+		nn.train();
 
-		ArrayList<Double> userInputs = new ArrayList<Double>();
-
-		WorkingNetwork nn = new WorkingNetwork(3, userInputs);
-		nn.parseCsv();
-
-		nn.setInputLayer(0);
-		nn.setHiddenLayerSizes();
-		nn.setTargets(0);
-		nn.forwardPropagate();
-		nn.backPropagate();
-
-		for (int i = 1; i < 1000; i++) {
-			nn.setInputLayer(i);
-			nn.setHiddenLayerSizes();
-			nn.setTargets(i);
-			nn.forwardPropagate();
-			nn.backPropagate();
-			// System.out.println(nn.vals.get(nn.numLayers - 1).get(0) + "new predicted
-			// output");
-			// System.out.println(nn.vals.get(nn.numLayers - 1).get(1) + "new predicted
-			// output");
-		}
 		System.out.println("-----Ready to Predict-----");
 		while (true) {
 			if (nn.scnr.next().charAt(0) == 'q') {
@@ -65,97 +44,155 @@ public class WorkingNetwork {
 	}
 
 	// this using arrayList for different sized hidden layers, flexible
-	public WorkingNetwork(int layers, ArrayList<Double> input) {
-		for (int i = 0; i < layers; i++) {
+	public WorkingNetwork(int layers) {
+		this.numLayers = layers;
+		for (int i = 0; i < numLayers; i++) {
 			vals.add(new ArrayList<Double>());
 		}
-		for (int i = 0; i < layers - 1; i++) {
+		for (int i = 0; i < numLayers - 1; i++) {
 			weights.add(new ArrayList<Double>());
 			deltas.add(new ArrayList<Double>());
 		}
 
-		this.numLayers = layers;
-		this.inputs = input;
+	}
+
+	public void train() throws IOException {
+		this.parseCsv();
+		setHiddenLayerSizes();
+
+		for (int i = 0; i < TRAINING_ROUNDS; i++) {
+			setInputLayer(i);
+			setTargets(i);
+			forwardPropagate();
+			backPropagate();
+			// System.out.println(nn.vals.get(nn.numLayers - 1).get(0) + "new predicted
+			// output");
+			// System.out.println(nn.vals.get(nn.numLayers - 1).get(1) + "new predicted
+			// output");
+		}
 	}
 
 	public void predict() {
 		Random rand = new Random();
 		int curIndex = rand.nextInt(300);
 		setInputLayer(curIndex);
-		setHiddenLayerSizes();
 		setTargets(curIndex);
 		forwardPropagate();
 		predictOutcome();
 	}
 
-	public void setInputLayer(int index) {
-		if (index == 0) {
-			for (int i = 0; i < trainingData.get(index).size(); i++) {
-				vals.get(0).add(0.0);
+	private List<List<Double>> parseCsv() throws IOException {
+//		csvFile = "C:/Users/wFanga/eclipse-workspace1/SimpleNN/MNIST/mnist_train.csv";
+		String trainingDataFilePath = "static/mnist_train.csv";
+		InputStream is = this.getClass().getClassLoader().getResourceAsStream(trainingDataFilePath);
+//		InputStream is = new java.util.zip.ZipInputStream(zFile);
+		Reader reader = new java.io.InputStreamReader(is);
+		CsvListReader cs = new CsvListReader(reader, CsvPreference.STANDARD_PREFERENCE);
+
+		int count;
+
+		try {
+			count = 0;
+			boolean isLabel;
+			while (count < 1000) {
+				trainingData.add(new ArrayList<Double>());
+				isLabel = true;
+				List<String> row = cs.read();
+				for (String s : row) {
+					if (isLabel) {
+						trainingData.get(count).add(Double.valueOf(s)); //
+						// System.out.print(Double.valueOf(s));
+					} else {
+						if (Double.valueOf(s) != 0) {
+							trainingData.get(count).add(1.0); //
+							// System.out.print(1.0);
+						} else {
+							trainingData.get(count).add(0.0); //
+							// System.out.print(0.0);
+						}
+					}
+					isLabel = false;
+				}
+				// System.out.println("");
+				count++;
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		for (int i = 0; i < trainingData.get(index).size(); i++) {
-			vals.get(0).set(i, trainingData.get(index).get(i));
-			// System.out.println(vals.get(0).get(i));
+		for (List<Double> d : trainingData) {
+			pairedTargets.add(d.get(0));
+			d.remove(0);
 		}
+		cs.close();
+		return trainingData;
 	}
 
-	public void setTargets(int index) {
-		if (index == 0) {
-			for (int i = 0; i < 10; i++) {
-				targets.add(0.0);
-			}
-		}
-		for (int i = 0; i < 10; i++) {
-			// double target = scnr.nextDouble();
-			if (i == pairedTargets.get(index)) {
-				targets.set(i, pairedTargets.get(index));
-			} else {
-				targets.set(i, 0.0);
-			}
-			System.out.println(targets.get(i));
-		}
-	}
-
-	public void setHiddenLayerSizes() {
+	private void setHiddenLayerSizes() {
 		Random rand = new Random(5);
 
-		for (int i = 1; i < this.numLayers; i++) { // setting hidden val sizes
+		for (int i = 0; i < this.neuronsInHiddenLayers.length; i++) { // setting hidden val sizes
 			// System.out.println("Input size");
 			// int size = scnr.nextInt();
-			vals.get(i).clear();
-			for (int j = 0; j < 50 - 20 * i; j++) {
-				vals.get(i).add(0.0);
+			vals.get(i + 1).clear();
+			for (int j = 0; j < neuronsInHiddenLayers[i]; j++) {
+				vals.get(i + 1).add(0.0);
 
 			}
-			for (int k = 0; k < vals.get(i - 1).size() * vals.get(i).size(); k++) {
-				weights.get(i - 1).add(rand.nextDouble());
-				deltas.get(i - 1).add(0.0);
+			for (int k = 0; k < vals.get(i).size() * vals.get(i + 1).size(); k++) {
+				weights.get(i).add(rand.nextDouble());
+				deltas.get(i).add(0.0);
 			}
 			// System.out.println(vals.get(i - 1).size() * vals.get(i).size());
 
 		}
 	}
 
-	public void forwardPropagate() {
-		for (int i = 1; i < numLayers; i++) {
-			double sum = 0;
-			// System.out.println(" ");
-			for (int j = 0; j < vals.get(i).size(); j++) {
-				for (int h = 0; h < vals.get(i - 1).size(); h++) {
-					sum += weights.get(i - 1).get(j * vals.get(i - 1).size() + h) * vals.get(i - 1).get(h);
+	private void setInputLayer(int rounds) {
+		vals.get(0).clear();
 
+		for (int i = 0; i < trainingData.get(rounds).size(); i++) {
+			vals.get(0).add(trainingData.get(rounds).get(i));
+			// System.out.println(vals.get(0).get(i));
+		}
+	}
+
+	private void setTargets(int rounds) {
+		targets.clear();
+
+		for (int i = 0; i < 10; i++) {
+			// double target = scnr.nextDouble();
+			if (i == pairedTargets.get(rounds)) {
+				targets.add(1.0);
+			} else {
+				targets.add(0.0);
+			}
+			// System.out.println(targets.get(i));
+		}
+	}
+
+	private void forwardPropagate() {
+		for (int i = 1; i < numLayers; i++) {
+			double sum = 0.0;
+			List<Double> currentLayer = vals.get(i);
+			List<Double> previousLayer = vals.get(i - 1);
+			List<Double> currentWeights = weights.get(i - 1);
+
+			// System.out.println(" ");
+			for (int currentLayerIndex = 0; currentLayerIndex < currentLayer.size(); currentLayerIndex++) {
+				for (int previousLayerIndex = 0; previousLayerIndex < previousLayer.size(); previousLayerIndex++) {
+					sum += currentWeights.get(currentLayerIndex * previousLayer.size() + previousLayerIndex)
+							* previousLayer.get(previousLayerIndex);
 				}
 				// System.out.println(sum + " this is the sum for" + i);
-				vals.get(i).set(j, 1.0 / (1.0 + Math.pow(Math.E, -sum / 100)));
+				currentLayer.set(currentLayerIndex, 1.0 / (1.0 + Math.pow(Math.E, -sum / 100.0)));
 				// System.out.println(" ");
 				// System.out.println(vals.get(i).get(j) + " = neuron(" + i + ")(" + j + ")");
-				sum = 0;
+				sum = 0.0;
 			}
 		}
 	}
 
-	public void backPropagate() {
+	private void backPropagate() {
 		for (int i = numLayers - 1; i > 0; i--) {
 			if (i == numLayers - 1) {
 				for (int j = 0; j < vals.get(i - 1).size(); j++) {
@@ -190,7 +227,7 @@ public class WorkingNetwork {
 		}
 	}
 
-	public double calculateAbsErr() {
+	private double calculateAbsErr() {
 		double sum = 0;
 
 		for (int i = 0; i < vals.get(numLayers - 1).size(); i++) {
@@ -199,10 +236,19 @@ public class WorkingNetwork {
 		return sum;
 	}
 
-	public void predictOutcome() {
+	private double outputErrorDeriv(double val, double target) {
+		return val - target; // -(target - val)
+	}
 
-		ArrayList<Double> listResults = new ArrayList<Double>();
-		for (int i = 0; i < 10; i++) {
+	private double sigmoidDeriv(double output) {
+
+		return output * (1 - output);
+	}
+
+	private void predictOutcome() {
+
+		List<Double> listResults = new ArrayList<Double>();
+		for (int i = 0; i < targets.size(); i++) {
 			listResults.add(0.5 * Math.pow(targets.get(i) - vals.get(numLayers - 1).get(i), 2));
 		}
 		// System.out.println(listResults);
@@ -218,59 +264,4 @@ public class WorkingNetwork {
 
 	}
 
-	public double outputErrorDeriv(double val, double target) {
-		return -(target - val);
-	}
-
-	public double sigmoidDeriv(double output) {
-
-		return output * (1 - output);
-	}
-
-	public ArrayList<ArrayList<Double>> parseCsv() throws IOException {
-		String
-
-//		csvFile = "C:/Users/wanga/eclipse-workspace1/SimpleNN/MNIST/mnist_train.csv";
-		trainingDataFilePath = "static/mnist_train.csv";
-		InputStream is = this.getClass().getClassLoader().getResourceAsStream(trainingDataFilePath);
-//		InputStream is = new java.util.zip.ZipInputStream(zFile);
-		Reader reader = new java.io.InputStreamReader(is);
-		CsvListReader cs = new CsvListReader(reader, CsvPreference.STANDARD_PREFERENCE);
-
-		int count;
-
-		try {
-			count = 0;
-			boolean isLabel;
-			while (count < 1000) {
-				trainingData.add(new ArrayList<Double>());
-				isLabel = true;
-				for (String s : cs.read()) {
-					if (isLabel) {
-						trainingData.get(count).add(Double.valueOf(s)); //
-						// System.out.print(Double.valueOf(s));
-					} else {
-						if (Double.valueOf(s) != 0) {
-							trainingData.get(count).add(1.0); //
-							// System.out.print(1.0);
-						} else {
-							trainingData.get(count).add(0.0); //
-							// System.out.print(0.0);
-						}
-					}
-					isLabel = false;
-				}
-				// System.out.println("");
-				count++;
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		for (ArrayList<Double> d : trainingData) {
-			pairedTargets.add(d.get(0));
-			d.remove(0);
-		}
-		cs.close();
-		return trainingData;
-	}
 }
